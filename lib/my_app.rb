b82f699377b56e6c0294393a8ab268e477836a55
MikaelFeher <mikael.feher@gmail.com>
2016-09-14 21:49:54 +0200

    Adds a new view and css

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -4,6 +4,9 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  get '/mikael' do
+    erb :mikael
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/my_app_spec.rb
+++ spec/features/my_app_spec.rb
@@ -3,7 +3,7 @@ describe 'home page' do
   it "displays text" do
     visit '/'
     expect(page.current_path).to eq '/'
-    expect(page).to have_content 'This is my index file'
+    expect(page).to have_content 'This is in my index file'
   end
 
   it "displays layout text" do
